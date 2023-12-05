bb9633708e59cf80557a1554138f71d563a8bf1e
Gurkirat Singh <63462904+SshgurkiratSingh@users.noreply.github.com>
2023-12-05 06:26:51 +0530

    feat

---
+++ ESP32Codes/THIRD_PANEL/src/main.cpp
@@ -0,0 +1,18 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <PubSubClient.h>
+// #include <Adafruit_SH1106.h>
+#include <Adafruit_SSD1306.h>
+#include <DHTesp.h>
+#include <WiFi.h>
+
+void setup()
+{
+  // put your setup code here, to run once:
+}
+
+void loop()
+{
+  // put your main code here, to run repeatedly:
+}
