5c33d7a6704ed4130dc4595a70472492335e133a
Gurkirat Singh <63462904+SshgurkiratSingh@users.noreply.github.com>
2023-12-11 22:44:03 +0530

    feat

--- ESP8266Codes/Pump_iot_enabled/src/main.cpp
+++ ESP8266Codes/Pump_iot_enabled/src/main.cpp
@@ -25,17 +25,74 @@ WiFiClient esp;
 #define MQTT_SERVER "ec2-3-88-49-62.compute-1.amazonaws.com"
 #define MQTT_PORT 1883
 PubSubClient client(esp);
+/**
+ * Displays the tank level by turning on LEDs according to the current level.
+ *
+ * @param None
+ *
+ * @return None
+ *
+ * @throws None
+ */
 void displayTankLevel()
 {
   // turn led on according to tank level
-   for (int i = 0; i < 4; i++){
-    if 
-   }
+  for (int i = 0; i < 4; i++)
+  {
+    if (currentLevel > i * 25)
+    {
+      digitalWrite(statusLedPin[i], HIGH);
+    }
+    else
+    {
+      digitalWrite(statusLedPin[i], LOW);
+    }
+  }
+}
+void callback(char *topic, byte *payload, unsigned int length)
+{
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++)
+  {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
 }
 void setup()
 {
+  Serial.begin(9600);
+  pinMode(RelayPin, OUTPUT);
+  pinMode(statusLedPin[0], OUTPUT);
+  pinMode(statusLedPin[1], OUTPUT);
+  pinMode(statusLedPin[2], OUTPUT);
+  pinMode(statusLedPin[3], OUTPUT);
+  pinMode(buttonPin, INPUT);
+  digitalWrite(RelayPin, LOW);
+  digitalWrite(statusLedPin[0], LOW);
+  digitalWrite(statusLedPin[1], LOW);
+  digitalWrite(statusLedPin[2], LOW);
+  digitalWrite(statusLedPin[3], LOW);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
 }
 
 void loop()
 {
+  if (digitalRead(buttonPin) == LOW)
+  {
+    pumpOn = !pumpOn;
+    delay(300);
+  }
+  currentLevel = sensor.ping_cm();
+  displayTankLevel();
+  if (pumpOn)
+  {
+    digitalWrite(RelayPin, HIGH);
+  }
+  else
+  {
+    digitalWrite(RelayPin, LOW);
+  }
+  client.loop();
 }
